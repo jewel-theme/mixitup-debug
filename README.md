a8bd4e28339224c00118491eeec86c952c622188
Patrick Kunka <patrick.kunka@barrelny.com>
2014-02-14 22:50:23 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+MixItUp Debug Extension
+=========
+
+A verbose debugging extension for MixItUp
+
+
+<br/>
+<hr/>
+
+*&copy; 2014 KunkaLabs Limited.*
---
+++ src/jquery.mixitup-debug.js
@@ -0,0 +1,28 @@
+ /**! 
+ * MixItUp Debug v1.0.0-beta
+ * A MixItUp Extension
+ *
+ * @copyright Copyright 2014 KunkaLabs Limited.
+ * @author    KunkaLabs Limited.
+ * @link      https://mixitup.kunkalabs.com
+ *
+ * @license   Commercial use requires a commercial license. 
+ *            https://mixitup.kunkalabs.com/licenses/
+ *
+ *            Non-commercial use permitted under terms of CC-BY-NC license.
+ *            http://creativecommons.org/licenses/by-nc/3.0/
+ */
+
+(function($, undf){
+	$.extend(true, $.MixItUp.prototype, {
+
+		/* Extend Action Hooks
+		---------------------------------------------------------------------- */
+
+		_actions: {
+			
+		},
+
+	});
+
+})(jQuery);
